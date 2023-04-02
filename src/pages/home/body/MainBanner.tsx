e2c809084862d3dc9d5ce6ee21e297e92a57600f
손주혜 <juhye.son990902@gmail.com>
2023-04-02 10:28:41 +0900

    feat: img태그가 아닌 background-image로 배너 삽입

--- src/pages/home/body/MainBanner.tsx
+++ src/pages/home/body/MainBanner.tsx
@@ -11,20 +11,17 @@ interface MainBannerProps {
 }
 export function MainBanner({ column, row, src }: MainBannerProps) {
     return (
-        <Wrapper column={column} row={row}>
-            <BannerImg src={src} />
+        <Wrapper src={src} column={column} row={row}>
         </Wrapper>
     )
 }
 
-const Wrapper = styled.div<{ column?: BannerCoordinates, row?: BannerCoordinates }>`
+const Wrapper = styled.div<{ src: string, column?: BannerCoordinates, row?: BannerCoordinates }>`
     grid-column-start: ${props => props.column?.start};
     grid-column-end: ${props => props.column?.end};
     grid-row-start: ${props => props.row?.start};
     grid-row-end: ${props => props.row?.end};
-`
-
-const BannerImg = styled.img`
-    width: 100%;
-    height: 100%;
+    background-image: url(${props => props.src});
+    background-size: cover;
+    object-fit: none;
 `
